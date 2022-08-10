7d012f6284521ef38bd323e72ed1e7f78ea262a5
LUCAS GUERRA <lucguerra99@hotmail.com>
2022-08-10 20:52:53 -0300

    Actualización

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,43 @@ import { GivenService } from './services/given.service';
 import { TurnosService } from './services/turnos.service';
 import { StartService } from './services/start.service';
 import { SoundsService } from './services/sounds.service';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  // content = document.getElementsByClassName('content');
+  
   constructor(public _mov: MovementsService,
               public _given: GivenService, 
               public _shifts: TurnosService,
               public _start: StartService,
-              public _sound: SoundsService
+              public _sound: SoundsService,
+              public config: NgbModalConfig, 
+              private modalService: NgbModal
               ) {
                 _sound.playMusic();
                }
-
- 
+               isOptionListVisible = false;
+               onTabPress($event:any) {
+                /*
+                Here you will press tab or shift+tab. 
+                In both the cases this.isOptionListVisible will be set to false.
+                <input
+                    type="text"
+                    (keydown)="onTabPress($event)"
+                    />
+                */
+                if ($event.key === 'm') {
+                  this.isOptionListVisible = !this.isOptionListVisible;
+                }
+                this.config.backdrop = 'static';
+                this.config.keyboard = false;
+              }
+              open(content: any) {
+                // console.log(this.content);
+                this.modalService.open(content);
+              }
 }
--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -4,90 +4,103 @@ export class Cards{
         type: 'escudo',
         name: 'Escudo de oro',
         info: 'Te cubre de 3 ataques',
-        valor: 3
+        valor: 3,
+        url: '../assets/escudodeoro.jpg'
     };
     2 = {
         id: 10001,
         type: 'escudo',
         name: 'Escudo de plata',
-        info: 'Te cubre de 2 ataques',
-        valor: 2
+        info: 'Te cubre de 1 ataques',
+        valor: 1,
+        url: '../assets/escudodebronce.jpg'
     };
     3 = {
         id: 10010,
         type: 'escudo',
         name: 'Escudo de bronce',
-        info: 'Te cubre de 1 ataque',
-        valor: 1
+        info: 'Te cubre de 2 ataque',
+        valor: 2,
+        url: '../assets/escudodeplata.jpg'
     };
     4 = {
         id: 10011,
         type: 'trampa',
         name: 'Trampa leve',
         info: 'Si caen en esta trampa roba 1 jugador',
-        valor: 1
+        valor: 1,
+        url: '../assets/tramparobarcarta.jpg'
     };
     5 = {
         id: 10100,
         type: 'trampa',
         name: 'Trampa safiro',
         info: 'Si caen en esta trampa le robas 1 carta',
-        valor: 1
+        valor: 1,
+        url: '../assets/tramparobarcarta.jpg'
     };
     6 = {
         id: 10101,
         type: 'trampa',
-        name: 'Espejito espejito',
+        name: 'Espejito',
         info: 'Si caen en esta trampa le devuelves el ataque',
-        valor: 1
+        valor: 1,
+        url: '../assets/espejito.jpg'
     };
     7 = {
         id: 10110,
         type: 'trampa',
         name: 'No mames wey x 2',
         info: 'Si caen en esta trampa le devuelves el ataque x 2',
-        valor: 2
+        valor: 2,
+        url: '../assets/espejito.jpg'
     };
     8 = {
         id: 10111,
         type: 'revivir',
         name: 'Revivir 2',
         info: 'Con esta carta revivis 2',
-        valor: 2
+        valor: 2,
+        url: '../assets/revivirdos.jpg'
     };
     9 = {
         id: 11000,
         type: 'revivir',
         name: 'Revivir todo el team',
         info: 'Con esta carta revivis a todo el equipo',
-        valor: 4
+        valor: 4,
+        url: '../assets/revivirteam.jpg'
     };
     10 = {
         id: 11001,
         type: 'ataque',
         name: 'Tira y mata',
         info: 'Mata segun el dado',
-        valor: 0
+        valor: 0,
+        url: '../assets/tiraymata.jpg'
     };
     11 = {
         id: 11010,
         type: 'ataque',
         name: 'Ataque duplicado',
         info: 'Con esta carta duplicas el ataque',
-        valor: 2
+        valor: 2,
+        url: '../assets/ataquedoble.jpg'
     };
     12 = {
         id: 11100,
         type: 'ataque',
         name: 'RAGNAROK',
         info: 'RAGNAROOOOOOOOOOOOOOOK (Mueren todos excepto 1 de cada equipo)',
-        valor: 4
+        valor: 4,
+        url: '../assets/ragnarok.jpg'
     };
     13 = {
         id: 11101,
         type: 'trampa',
         name: "Trampa mortal",
         info: 'Si caes en esta trampa, se te muere 1',
-        valor: 1
+        valor: 1,
+        url: '../assets/trampamatar.jpg'
     }
 };
